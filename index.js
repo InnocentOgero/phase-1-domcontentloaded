86b87077961037dc2c68237fa6e0ba4032b7bec0
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-10-02 18:38:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const words= document.querySelector('p');
+    if (words) {
+      words.textContent = 'This is really cool!';
+    }
+  });
